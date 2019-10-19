c8afd2a3c1cc671b32223aec8cebb7e968e4879f
Griffin Cosgrove <griffcosgrove@gmail.com>
2019-10-19 19:13:00 -0400

    moving files n stuff

---
+++ App_Code/PropertyAmenity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+public class PropertyAmenity
+{
+    private int propertyID;
+    private string amenity;
+
+	public PropertyAmenity(int propertyID, string amenity)
+	{
+        setPropertyID(propertyID);
+        setAmenity(amenity);
+	}
+
+    public void setPropertyID(int propertyID)
+    {
+        this.propertyID = propertyID;
+    }
+    public int getPropertyID()
+    {
+        return this.propertyID;
+    }
+    public void setAmenity(string amenity)
+    {
+        this.amenity = amenity;
+    }
+
+
+
+    public string getAmenity()
+    {
+        return this.amenity;
+    }
+
+}
