2cf9c6bb1c39515a9f9e5634e66dd59433280014
Griffin Cosgrove <griffcosgrove@gmail.com>
2019-10-22 01:17:51 -0400

    added cross site scripting prevention and some small changes

--- RoomMagnetTake1/hostform1.aspx.cs
+++ RoomMagnetTake1/hostform1.aspx.cs
@@ -4,11 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
 
 public partial class hostform1 : System.Web.UI.Page
 {
+    // connection string at the class level so we can reference it later. this is the string in the web.config !!
+    SqlConnection sc = new SqlConnection(ConfigurationManager.ConnectionStrings["cs"].ConnectionString);
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
     }
+
+
+
+    protected void btnCommitHost_Click(object sender, EventArgs e)
+    {
+        //Host newHost = new Host(getString(txtFirstName), getString(txtLastName), getString(txtEmail), getString(txtPhoneNumber), getString(txtAddress), getString(txt)
+    }
+
+    public String getString(TextBox txt)
+    {
+        String returnString = HttpUtility.HtmlEncode(txt.Text);
+        return returnString;
+    }
+
 }
