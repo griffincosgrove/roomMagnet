bdc61a614cb5f89c17a62920fed9355527bd08c6
Ayman Mohammed <moham2aaaa@gmail.com>
2019-10-22 00:46:31 -0400

    login files'

---
+++ RoomMagnetTake1/App_Code/PasswordHash.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography; // must be used
+using System.Web;
+
+public class PasswordHash
+{
+    public const int SaltByteSize = 24; // standard, secure size of salts
+    public const int HashByteSize = 20; // to match the size of the PBKDF2-HMAC-SHA-1 hash (standard)
+    public const int Pbkdf2Iterations = 1000; // higher number is more secure but takes longer
+    public const int IterationIndex = 0; // used to find first section (number of iterations) of PasswordHash database field
+    public const int SaltIndex = 1; // used to find second section (salt) of PasswordHash database field
+    public const int Pbkdf2Index = 2; // used to find third section (hash) of PasswordHash database field
+
+    public static string HashPassword(string password)
+    {
+        var cryptoProvider = new RNGCryptoServiceProvider(); // create a new crypto provider
+        byte[] salt = new byte[SaltByteSize]; // creates a new random salt of a certain length
+        cryptoProvider.GetBytes(salt); // fills array with cryptographically strong sequence of random values
+
+        var hash = GetPbkdf2Bytes(password, salt, Pbkdf2Iterations, HashByteSize); // call method below to create the hash
+        return Pbkdf2Iterations + ":" + Convert.ToBase64String(salt) + ":" + Convert.ToBase64String(hash); // create string to store in database and return
+    }
+
+    private static byte[] GetPbkdf2Bytes(string password, byte[] salt, int iterations, int outputBytes)
+    {
+        var pbkdf2 = new Rfc2898DeriveBytes(password, salt); // create a new key
+        pbkdf2.IterationCount = iterations; // assign number of iterations that the function is run
+        return pbkdf2.GetBytes(outputBytes); // return pseudo-random hash of certain length
+    }
+
+    public static bool ValidatePassword(string password, string correctHash)
+    {
+        char[] delimiter = { ':' }; // this section takes the whole stored string and splits it up into the 3 parts
+        var split = correctHash.Split(delimiter); // splits the long string at the : character
+        var iterations = Int32.Parse(split[IterationIndex]); // picks out the first section and assigns the stored number of iterations to new variable
+        var salt = Convert.FromBase64String(split[SaltIndex]); // picks out the second section and assign stored salt to new variable
+        var hash = Convert.FromBase64String(split[Pbkdf2Index]); // picks out the third section and assign stored password hash to new variable
+
+        var testHash = GetPbkdf2Bytes(password, salt, iterations, hash.Length); // creates the hash for the entered password
+        return SlowEquals(hash, testHash); // compare the stored password (hash) to the entered password (testhash) and return true (matches) or false (doesn't)
+    }
+
+    private static bool SlowEquals(byte[] a, byte[] b) // optional method -> increases security/makes password cracking take longer
+    {
+        var diff = (uint)a.Length ^ (uint)b.Length;
+        for (int i = 0; i < a.Length && i < b.Length; i++)
+        {
+            diff |= (uint)(a[i] ^ b[i]);
+        }
+        return diff == 0;
+    }
+}
---
+++ RoomMagnetTake1/login.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class login : System.Web.UI.Page
+{
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        // connect to database to retrieve stored password string
+        try
+        {
+                SqlConnection sc = new SqlConnection(ConfigurationManager.ConnectionStrings["cs"].ConnectionString);
+
+           
+            lblStatus.Text = "Database Connection Successful";
+
+            sc.Open();
+            System.Data.SqlClient.SqlCommand findPass = new System.Data.SqlClient.SqlCommand();
+            findPass.Connection = sc;
+            // SELECT PASSWORD STRING WHERE THE ENTERED USERNAME MATCHES
+            findPass.CommandText = "select password from TENANT where Email = @Email";
+            findPass.Parameters.Add(new SqlParameter("@Email", txtUsername.Text));
+
+            SqlDataReader reader = findPass.ExecuteReader(); // create a reader
+
+            if (reader.HasRows) // if the username exists, it will continue
+            {
+                while (reader.Read()) // this will read the single record that matches the entered username
+                {
+                    string storedHash = reader["password"].ToString(); // store the database password into this variable
+
+                    if (PasswordHash.ValidatePassword(txtPassword.Text, storedHash)) // if the entered password matches what is stored, it will show success
+                    {
+                        lblStatus.Text = "Success!";
+                        btnLogin.Enabled = false;
+                        txtUsername.Enabled = false;
+                        txtPassword.Enabled = false;
+                        Session["USER_ID"] = txtUsername.Text;
+                        Response.Redirect("hostdashboard.aspx");
+                    }
+                    else
+                        lblStatus.Text = "Password is wrong.";
+                }
+            }
+            else // if the username doesn't exist, it will show failure
+                lblStatus.Text = "Login failed.";
+
+            sc.Close();
+        }
+        catch
+        {
+            lblStatus.Text = "Database Error.";
+        }
+
+    }
+
+    protected void lnkCreate_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("createUser.aspx", false);
+    }
+}
