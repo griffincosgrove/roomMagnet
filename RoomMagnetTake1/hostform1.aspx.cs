849663c3e1d64ac1454e45789d00eb42d65934b8
Griffin Cosgrove <griffcosgrove@gmail.com>
2019-10-25 02:18:33 -0400

    fixed a couple things, add dropdown for state in host1, sql for host

--- RoomMagnetTake1/hostform1.aspx.cs
+++ RoomMagnetTake1/hostform1.aspx.cs
@@ -21,7 +21,45 @@ public partial class hostform1 : System.Web.UI.Page
 
     protected void btnCommitHost_Click(object sender, EventArgs e)
     {
-        //Host newHost = new Host(getString(txtFirstName), getString(txtLastName), getString(txtEmail), getString(txtPhoneNumber), getString(txtAddress), getString(txt)
+        Host newHost = new Host(getString(txtFirstName), getString(txtLastName), ddGender.Text, getString(txtEmail), getString(txtPhoneNumber),
+            getString(txtStreet), getString(txtCity), ddState.Text, getString(txtZip), convertToDateFormat(combineBirthday()));
+
+        SqlCommand command = new SqlCommand("INSERT into [dbo].[Host] (FirstName, LastName, Gender, Email, Password, PhoneNumber, HostAddress, City, ZipCode, State, BirthDate) " +
+            "VALUES(@firstName, @lastName, @gender, @email, @password, @phoneNumber, @hostAddress, @city, @zipcode, @state, @dateOfBirth)", sc);
+
+        command.Parameters.AddWithValue("@firstName", newHost.getFirstName());
+        command.Parameters.AddWithValue("@lastName", newHost.getLastName());
+        command.Parameters.AddWithValue("@gender", newHost.getGender());
+        command.Parameters.AddWithValue("@email", newHost.getEmail());
+        command.Parameters.AddWithValue("@password", PasswordHash.HashPassword(txtPassword.Text));
+        command.Parameters.AddWithValue("@phoneNumber", newHost.getPhoneNumber());
+        command.Parameters.AddWithValue("@hostAddress", newHost.getStreetAddress());
+        command.Parameters.AddWithValue("@city", newHost.getCity());
+        command.Parameters.AddWithValue("@state", newHost.getState());
+        command.Parameters.AddWithValue("@zipcode", newHost.getZipcode());
+        command.Parameters.AddWithValue("@dateOfBirth", newHost.getDateOfBirth());
+
+        try
+        {
+            sc.Open();
+            command.ExecuteNonQuery();
+            //ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "NoDatabaseAlertMessage", "alert('New Host inserted')", true);
+        }
+        catch
+        {
+            //ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "NoDatabaseAlertMessage", "alert('Host NOT Inserted')", true);
+        }
+        finally
+        {
+            sc.Close();
+        }
+        
+
+
+
+
+
+
         Response.Redirect("hostform2.aspx");
     }
 
@@ -29,6 +67,18 @@ public partial class hostform1 : System.Web.UI.Page
     {
         String returnString = HttpUtility.HtmlEncode(txt.Text);
         return returnString;
+    }
+
+    protected DateTime convertToDateFormat(String date)
+    {
+        DateTime parseValue = DateTime.Parse(date);
+        return parseValue;
+    }
+
+    protected String combineBirthday()
+    {
+        String str = (ddMonth.Text + " " + ddDay.Text + ", " + ddYear.Text);
+        return str;
     }
 
 }
--- RoomMagnetTake1/hostform2.aspx.cs
+++ RoomMagnetTake1/hostform2.aspx.cs
@@ -11,4 +11,6 @@ public partial class hostform2 : System.Web.UI.Page
     {
 
     }
+
+
 }
