5d6545e80194e6b601a9693f37950b269321033b
Griffin Cosgrove <griffcosgrove@gmail.com>
2019-10-27 21:11:26 -0400

    add class and fix insert to host

---
+++ RoomMagnetTake1/App_Code/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class Customer
+{
+    private String email;
+    public Customer(String email)
+    {
+        setEmail(email);
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getEmail()
+    {
+        return this.email;
+    }
+}
